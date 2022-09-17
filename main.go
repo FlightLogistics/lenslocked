14d487eecbf6ea0c63af069f4fc1e56bfe227b2f
Jonathan Binnie <jb@flightlogistics.co.uk>
2022-09-17 16:36:36 +0000

    Added a line about the GitHub site

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!  THIS WAS DONE ON THE SITE IN DROPBOX</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!  THIS WAS DONE FROM THE GITHUB SITE</h1>")
 }
 
 // Added some comments
